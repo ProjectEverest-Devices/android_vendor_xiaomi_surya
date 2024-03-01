3e3c426b2a4329f187f03e66f13c38375f65cbf2
root <root@DESKTOP-5BJSGF4>
2024-03-01 22:59:17 +0700

    Add prebuilt libprotobuf-cpp-lite-3.9.1 from redfin

--- surya-vendor.mk
+++ surya-vendor.mk
@@ -629,6 +629,7 @@ PRODUCT_PACKAGES += \
     libpdmapper \
     libpdnotifier \
     libperipheral_client \
+    libprotobuf-cpp-lite-3.9.1 \
     libqcbor \
     libqcc_file_agent \
     libqcmaputils \
